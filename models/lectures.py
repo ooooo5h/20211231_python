ded9134c778c1d457d43ed096d3edd8153bcaabf
ooooo5h <ooooo5h91@gmail.com>
2021-12-31 16:17:44 +0900

    round기능으로 소수 첫번째자리에서 반올림

--- models/lectures.py
+++ models/lectures.py
@@ -2,4 +2,6 @@ class Lectures:
     
     def __init__(self, data_dict):
         self.name = data_dict['name']
-        self.avg_score = data_dict['avg_score']        
+        
+        # 평균점수 : DB에서 긴 소수점이 와도, 파이썬에서 소수 1자리까지만 넣어주자
+        self.avg_score = round(data_dict['avg_score'], 1) 
