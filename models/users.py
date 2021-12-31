32d264d3ab52113b72e1bfbb1e156784d839a2d4
ooooo5h <ooooo5h91@gmail.com>
2021-12-31 11:08:02 +0900

    datetime 모듈 - now() 활용

--- models/users.py
+++ models/users.py
@@ -1,6 +1,9 @@
 # users 테이블을 표현하는 용도
 # DB의 테이블을 프로그램에서 표현하는 클래스들을 model이라고 부른다
 
+# 일시 관련 기능들이 모여있는 모듈 불러내기
+import datetime
+
 class Users:
     
     def __init__(self, info_dict):
@@ -17,7 +20,7 @@ class Users:
         
     # 각 사용자의 정보를 가공해서 간략하게 출력하는 기능
     def get_simple_info(self):
-        print(f'{self.name} - {self.get_age}세, {self.gender}')
+        print(f'{self.name} - {self.get_age()}세, {self.gender}')
         
     
     # 사용자의 나이 계산해주는 기능
@@ -25,4 +28,6 @@ class Users:
         
         # 오늘은 12월 31일이니까 내일부터 나이가 바껴 ㅠㅠ
         # 현재 일자의 연도를 얻어오면, 자동으로 현재 나이 계산이 가능하겠지?
-        return 2021-{self.birth_year}+1
+        
+        now_year = datetime.datetime.now().year
+        return now_year - self.birth_year + 1
